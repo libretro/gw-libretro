be91157087764b0e18be575bc0d1641854c6b160
Andre Leiradella <leiradel@debian>
2015-04-30 16:56:45 -0300

    added windows cross compile and mingw32 native compile

--- etc/mkmake.lua
+++ etc/mkmake.lua
@@ -1,11 +1,18 @@
 local template = [[
+##############
+# ${HEADERMSG}
+
+#############
+# User config
+
 DEBUG = 0
 LOG_PERFORMANCE = 1
 
-
+####################################
+# Variable setup for Makefile.common
 
 CORE_DIR      = ..
-INCDIRS       = -I${PLAT_INCDIR}
+INCLUDES      = -I${PLAT_INCDIR}
 SOURCES_C     =
 SOURCES_CXX   =
 SOURCES_LUA_C =
@@ -14,114 +21,74 @@ PNG_FILES     =
 
 include Makefile.common
 
-
+#################
+# Toolchain setup
 
 CC  = ${CC}
 CXX = ${CXX}
 AS  = ${AS}
 
-
+############
+# Extensions
 
 OBJEXT = .${EXT}.o
 SOEXT  = .${EXT}.${SO}
-TARGET = gw_libretro$(SOEXT)
 
-
+################
+# Platform setup
 
 PLATDEFS     = ${PLAT_DEFS}
 PLATCFLAGS   = ${PLAT_CFLAGS}
 PLATCXXFLAGS = ${PLAT_CXXFLAGS}
 PLATLDFLAGS  = ${PLAT_LDFLAGS}
 
-
+################
+# libretro setup
 
 RETRODEFS     = -D__LIBRETRO__
 RETROCFLAGS   =
 RETROCXXFLAGS =
 RETROLDFLAGS  =
 
+#################
+# Final variables
 
+DEFINES  = $(PLATDEFS) $(RETRODEFS)
+CFLAGS   = $(PLATCFLAGS) $(RETROCFLAGS) $(DEFINES) $(INCLUDES)
+CXXFLAGS = $(PLATCXXFLAGS) $(RETROCXXFLAGS) $(DEFINES) $(INCLUDES)
+LDFLAGS  = $(PLATLDFLAGS) $(RETROLDFLAGS)
 
-COREDEFS     = -Drl_malloc=malloc -Drl_free=free -Dgwlua_malloc=malloc -Dgwlua_realloc=realloc -Dgwlua_free=free -Dgwrom_malloc=malloc -Dgwrom_free=free
-CORECFLAGS   = -Wall --std=c99
-CORECXXFLAGS = -Wall
-CORELDFLAGS  =
-
-
-
-DEFINES  = $(PLATDEFS) $(RETRODEFS) $(COREDEFS)
-CFLAGS   = $(PLATCFLAGS) $(RETROCFLAGS) $(CORECFLAGS) $(DEFINES) $(INCDIRS)
-CXXFLAGS = $(PLATCXXFLAGS) $(RETROCXXFLAGS) $(CORECXXFLAGS) $(DEFINES) $(INCDIRS)
-LDFLAGS  = $(PLATLDFLAGS) $(RETROLDFLAGS) $(CORELDFLAGS) -shared
-
-
+########
+# Tuning
 
 ifeq ($(DEBUG), 1)
-	CFLAGS   += -O0 -g
-	CXXFLAGS += -O0 -g
+  CFLAGS   += -O0 -g
+  CXXFLAGS += -O0 -g
   LDFLAGS  += -g
 else
-	CFLAGS   += -O3 -DNDEBUG
-	CXXFLAGS += -O3 -DNDEBUG
+  CFLAGS   += -O3 -DNDEBUG
+  CXXFLAGS += -O3 -DNDEBUG
 endif
 
 ifeq ($(LOG_PERFORMANCE), 1)
-	CFLAGS   += -DLOG_PERFORMANCE
-	CXXFLAGS += -DLOG_PERFORMANCE
+  CFLAGS   += -DLOG_PERFORMANCE
+  CXXFLAGS += -DLOG_PERFORMANCE
 endif
 
+###############
+# Include rules
 
-
-HEADERS = $(LUA_FILES:.lua=.h) $(PNG_FILES:.png=.h)
-OBJS    = $(SOURCES_C:.c=$(OBJEXT)) $(SOURCES_CXX:.cpp=$(OBJEXT)) $(SOURCES_LUA_C:.c=$(OBJEXT)) $(CORE_DIR)/android/missing$(OBJEXT)
-
-%$(OBJEXT): %.cpp
-	$(CXX) -c -o $@ $< $(CXXFLAGS) $(DEFINES) $(INCDIRS)
-
-%$(OBJEXT): %.c
-	$(CC) -c -o $@ $< $(CFLAGS) $(DEFINES) $(INCDIRS)
-
-%$(OBJEXT): %.S
-	$(AS) -c -o $@ $< $(CFLAGS) $(DEFINES) $(INCDIRS)
-
-%.h: %.lua
-	xxd -i $< | sed "s/unsigned/const unsigned/g" > $@
-
-%.h: %.png
-	retroluxury/etc/luai/luai retroluxury/etc/rlrle.lua $<
-	xxd -i $(basename $<).rle | sed "s/unsigned/const unsigned/g" > $@
-	rm -f $(basename $<).rle
-
-all: $(TARGET)
-
-$(TARGET): $(HEADERS) $(OBJS)
-	$(CXX) -o $@ $(OBJS) $(LDFLAGS)
-
-$(CORE_DIR)/gwlua/functions.c: $(CORE_DIR)/gwlua/entries.c $(HEADERS)
-	touch $@
-
-$(CORE_DIR)/gwlua/entries.c: $(CORE_DIR)/gwlua/entries.gperf
-	gperf -c -C -l -L C -t -7 -m 100 -I --output-file=$@ $<
-
-clean-objs:
-	rm -f $(OBJS)
-
-clean: clean-objs
-	rm -f $(TARGET)
-
-dist-clean: clean
-	rm -f $(HEADERS)
-	rm -f $(CORE_DIR)/gwlua/entries.c
-
-.PHONY: $(TARGET) clean-objs clean dist-clean
+include Makefile.rules
 ]]
 
-local host = 'linux-x86'
+local host = 'linux-x86_64'
+--local host = 'linux-x86'
 --local host = 'windows-x86_64'
 
 local platforms = {
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_arm64_v8a = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-g++',
     AS            = '$(NDK_ROOT)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-as',
@@ -135,6 +102,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_x86_64 = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-g++',
     AS            = '$(NDK_ROOT)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-as',
@@ -148,6 +116,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_mips64 = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-g++',
     AS            = '$(NDK_ROOT)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-as',
@@ -161,6 +130,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_arm_v7a = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-g++',
     AS            = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-as',
@@ -174,6 +144,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_arm_v5te = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-g++',
     AS            = '$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-as',
@@ -187,6 +158,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_x86 = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-g++',
     AS            = '$(NDK_ROOT)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-as',
@@ -200,6 +172,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   android_mips = {
+    HEADERMSG     = 'Download android-ndk-r10d-linux-x86_64.bin from https://developer.android.com/tools/sdk/ndk/index.html, unpack somewhere, and set NDK_ROOT to it',
     CC            = '$(NDK_ROOT)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-gcc',
     CXX           = '$(NDK_ROOT)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-g++',
     AS            = '$(NDK_ROOT)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-as',
@@ -213,6 +186,7 @@ local platforms = {
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   linux_x86 = {
+    HEADERMSG     = '',
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
@@ -222,10 +196,11 @@ local platforms = {
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m32 -fpic -fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-lm'
+    PLAT_LDFLAGS  = '-m32 -shared -lm'
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   linux_x86_64 = {
+    HEADERMSG     = '',
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
@@ -235,10 +210,11 @@ local platforms = {
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m64 -fpic -fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-lm'
+    PLAT_LDFLAGS  = '-m64 -shared -lm'
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   windows_x86 = {
+    HEADERMSG     = 'Download mingw-w32-bin_x86_64-linux_20131227.tar.bz2 from http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Automated%20Builds/, unpack somewhere, and set MINGW32 to it',
     CC            = '$(MINGW32)/bin/i686-w64-mingw32-gcc',
     CXX           = '$(MINGW32)/bin/i686-w64-mingw32-g++',
     AS            = '$(MINGW32)/bin/i686-w64-mingw32-as',
@@ -246,12 +222,13 @@ local platforms = {
     SO            = 'dll',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
-    PLAT_CFLAGS   = '-fpic -fstrict-aliasing',
+    PLAT_CFLAGS   = '-fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-lm'
+    PLAT_LDFLAGS  = '-shared -lm'
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   windows_x86_64 = {
+    HEADERMSG     = 'Download mingw-w64-bin_x86_64-linux_20131228.tar.bz2 from http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Automated%20Builds/, unpack somewhere, and set MINGW64 to it',
     CC            = '$(MINGW64)/bin/x86_64-w64-mingw32-gcc',
     CXX           = '$(MINGW64)/bin/x86_64-w64-mingw32-g++',
     AS            = '$(MINGW64)/bin/x86_64-w64-mingw32-as',
@@ -261,7 +238,21 @@ local platforms = {
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-fpic -fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-lm'
+    PLAT_LDFLAGS  = '-shared -lm'
+  },
+  -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  mingw32 = {
+    HEADERMSG     = '',
+    CC            = 'gcc',
+    CXX           = 'g++',
+    AS            = 'as',
+    EXT           = 'mingw32',
+    SO            = 'dll',
+    PLAT_INCDIR   = '',
+    PLAT_DEFS     = '',
+    PLAT_CFLAGS   = '-fpic -fstrict-aliasing',
+    PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
+    PLAT_LDFLAGS  = '-shared -lm'
   },
 }
 
@@ -273,7 +264,7 @@ for plat, defs in pairs( platforms ) do
     equal = true
     
     for def, value in pairs( defs ) do
-      local templ2 = templ:gsub( '%${' .. def .. '}', value )
+      local templ2 = templ:gsub( '%${' .. def .. '}', ( value:gsub( '%%', '%%%%' ) ) )
       equal = equal and templ == templ2
       templ = templ2
     end
