0f9c86b053a218e83d459a5b32e43a68eee5875c
Andre Leiradella <andre@leiradella.com>
2015-07-26 01:30:46 -0300

    leave mkmake.lua and the generated makefiles as core-independent as possible

--- etc/mkmake.lua
+++ etc/mkmake.lua
@@ -11,13 +11,8 @@ LOG_PERFORMANCE = 1
 ####################################
 # Variable setup for Makefile.common
 
-CORE_DIR      = ..
-INCLUDES      = ${PLAT_INCDIR}
-SOURCES_C     =
-SOURCES_CXX   =
-SOURCES_LUA_C =
-LUA_FILES     =
-PNG_FILES     =
+CORE_DIR  = ..
+INCLUDES  = ${PLAT_INCDIR}
 
 include Makefile.common
 
@@ -27,6 +22,7 @@ include Makefile.common
 CC  = ${CC}
 CXX = ${CXX}
 AS  = ${AS}
+AR  = ${AR}
 
 ############
 # Extensions
@@ -37,10 +33,12 @@ SOEXT  = .${EXT}.${SO}
 ################
 # Platform setup
 
-PLATDEFS     = ${PLAT_DEFS}
-PLATCFLAGS   = ${PLAT_CFLAGS}
-PLATCXXFLAGS = ${PLAT_CXXFLAGS}
-PLATLDFLAGS  = ${PLAT_LDFLAGS}
+STATIC_LINKING = ${STATIC_LINKING}
+PLATFORM       = ${PLATFORM}
+PLATDEFS       = ${PLAT_DEFS}
+PLATCFLAGS     = ${PLAT_CFLAGS}
+PLATCXXFLAGS   = ${PLAT_CXXFLAGS}
+PLATLDFLAGS    = ${PLAT_LDFLAGS}
 
 ################
 # libretro setup
@@ -93,10 +91,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/aarch64-linux-android-4.9/prebuilt/' .. host .. '/bin/aarch64-linux-android-ar',
     EXT           = 'android_arm64_v8a',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-21/arch-arm64/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-fpic -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-21/arch-arm64 -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -108,10 +108,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/x86_64-4.9/prebuilt/' .. host .. '/bin/x86_64-linux-android-ar',
     EXT           = 'android_x86_64',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-21/arch-x86_64/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-21/arch-x86_64 -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -123,10 +125,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/mips64el-linux-android-4.9/prebuilt/' .. host .. '/bin/mips64el-linux-android-ar',
     EXT           = 'android_mips64',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-21/arch-mips64/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-fpic -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers -no-canonical-prefixes -fomit-frame-pointer -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-21/arch-mips64 -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -138,10 +142,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-ar',
     EXT           = 'armeabi-v7a',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-3/arch-arm/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-fpic -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-3/arch-arm -lgcc -no-canonical-prefixes -march=armv7-a -Wl,--fix-cortex-a8 -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -153,10 +159,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/arm-linux-androideabi-4.8/prebuilt/' .. host .. '/bin/arm-linux-androideabi-ar',
     EXT           = 'armeabi',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-3/arch-arm/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-fpic -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes -march=armv5te -mtune=xscale -msoft-float -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-3/arch-arm -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -168,10 +176,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/x86-4.8/prebuilt/' .. host .. '/bin/i686-linux-android-ar',
     EXT           = 'x86',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-9/arch-x86/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-ffunction-sections -funwind-tables -no-canonical-prefixes -fstack-protector -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-9/arch-x86 -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -183,10 +193,12 @@ local platforms = {
     CC            = '$(NDK_ROOT_DIR)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-gcc',
     CXX           = '$(NDK_ROOT_DIR)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-g++',
     AS            = '$(NDK_ROOT_DIR)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-as',
+    AR            = '$(NDK_ROOT_DIR)/toolchains/mipsel-linux-android-4.8/prebuilt/' .. host .. '/bin/mipsel-linux-android-ar',
     EXT           = 'android_mips',
     SO            = 'so',
+    PLATFORM      = 'android',
     PLAT_INCDIR   = '-I$(NDK_ROOT_DIR)/platforms/android-9/arch-mips/usr/include',
-    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST -Dl_getlocaledecpoint\\(\\)=\\(\\\'.\\\'\\)',
+    PLAT_DEFS     = '-DANDROID -DINLINE=inline -DHAVE_STDINT_H -DBSPF_UNIX -DHAVE_INTTYPES -DLSB_FIRST',
     PLAT_CFLAGS   = '-fpic -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers -no-canonical-prefixes -fomit-frame-pointer -funswitch-loops -finline-limit=300 -Wa,--noexecstack -Wformat -Werror=format-security',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
     PLAT_LDFLAGS  = '-shared --sysroot=$(NDK_ROOT_DIR)/platforms/android-9/arch-mips -lgcc -no-canonical-prefixes -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -lc -lm'
@@ -198,8 +210,10 @@ local platforms = {
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
+    AR            = 'ar',
     EXT           = 'linux_x86',
     SO            = 'so',
+    PLATFORM      = 'unix',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m32 -fpic -fstrict-aliasing',
@@ -213,8 +227,10 @@ local platforms = {
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
+    AR            = 'ar',
     EXT           = 'linux_x86_64',
     SO            = 'so',
+    PLATFORM      = 'unix',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m64 -fpic -fstrict-aliasing',
@@ -228,8 +244,10 @@ local platforms = {
     CC            = 'i686-w64-mingw32-gcc',
     CXX           = 'i686-w64-mingw32-g++',
     AS            = 'i686-w64-mingw32-as',
+    AR            = 'i686-w64-mingw32-ar',
     EXT           = 'windows_x86',
     SO            = 'dll',
+    PLATFORM      = 'win',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-fstrict-aliasing',
@@ -243,8 +261,10 @@ local platforms = {
     CC            = 'x86_64-w64-mingw32-gcc',
     CXX           = 'x86_64-w64-mingw32-g++',
     AS            = 'x86_64-w64-mingw32-as',
+    AR            = 'x86_64-w64-mingw32-ar',
     EXT           = 'windows_x86_64',
     SO            = 'dll',
+    PLATFORM      = 'win',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-fpic -fstrict-aliasing',
@@ -254,38 +274,62 @@ local platforms = {
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   mingw32 = {
     MAKEFILE      = 'Makefile.mingw32',
-    HEADERMSG     = '',
+    HEADERMSG     = 'Install MSYS2',
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
+    AR            = 'ar',
     EXT           = 'mingw32',
     SO            = 'dll',
+    PLATFORM      = 'win',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m32 -fpic -fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-m32 -shared -lm'
+    PLAT_LDFLAGS  = '-shared -lm'
   },
   -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   mingw64 = {
     MAKEFILE      = 'Makefile.mingw64',
-    HEADERMSG     = 'Install tdm-gcc (http://tdm-gcc.tdragon.net/)',
+    HEADERMSG     = 'Install MSYS2',
     CC            = 'gcc',
     CXX           = 'g++',
     AS            = 'as',
-    EXT           = 'mingw64',
+    AR            = 'ar',
+    EXT           = 'mingw32',
     SO            = 'dll',
+    PLATFORM      = 'win',
     PLAT_INCDIR   = '',
     PLAT_DEFS     = '',
     PLAT_CFLAGS   = '-m64 -fpic -fstrict-aliasing',
     PLAT_CXXFLAGS = '${PLAT_CFLAGS}',
-    PLAT_LDFLAGS  = '-m64 -shared -lm'
+    PLAT_LDFLAGS  = '-shared -lm'
+  },
+  -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  wii = {
+    MAKEFILE       = 'Makefile.wii',
+    HEADERMSG      = 'Install devkitppc and libogc',
+    CC             = '$(DEVKITPPC_ROOT_DIR)/bin/powerpc-eabi-gcc',
+    CXX            = '$(DEVKITPPC_ROOT_DIR)/bin/powerpc-eabi-g++',
+    AS             = '$(DEVKITPPC_ROOT_DIR)/bin/powerpc-eabi-as',
+    AR             = '$(DEVKITPPC_ROOT_DIR)/bin/powerpc-eabi-ar',
+    EXT            = 'wii',
+    SO             = 'so',
+    PLATFORM       = 'wii',
+    PLAT_INCDIR    = '',
+    PLAT_DEFS      = '-DGEKKO -DHW_RVL',
+    PLAT_CFLAGS    = '-m32 -fstrict-aliasing -mrvl -mcpu=750 -meabi -mhard-float',
+    PLAT_CXXFLAGS  = '${PLAT_CFLAGS}',
+    PLAT_LDFLAGS   = '-shared -lm',
+    STATIC_LINKING = '1'
   },
 }
 
 for plat, defs in pairs( platforms ) do
   local templ = template
   local equal
+
+  defs.STATIC_LINKING = defs.STATIC_LINKING or '0'
   
   repeat
     equal = true
