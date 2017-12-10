750ce41ee9208b5a5c554180b4560f8883adbbe2
leiradel <andre@leiradella.com>
2017-12-10 13:04:24 +0000

    Updated Condor to support mouse and touches

--- games/condor/main.lua
+++ games/condor/main.lua
@@ -1,30 +1,8 @@
 local unit1 = system.loadunit 'unit1'
 local forms = system.loadunit 'forms'
+local controls = system.loadunit 'controls'
 
-local keymap = {
-  left  = { forms.vk_left,  'Left' },
-  right = { forms.vk_right, 'Right' },
-  a     = { forms.vk_right, 'Right' },
-  l1    = { 49,             'Game A' },
-  r1    = { 50,             'Game B' },
-  l2    = { 51,             'Time' }
-}
-
-local menu = {
-  { unit1.form1.btn_game_a_down, 'Game A', 49 },
-  { unit1.form1.btn_game_b_down, 'Game B', 50 },
-  { unit1.form1.btn_mode_down,   'Time',   51 }
-}
-
-local timers = {
-  unit1.form1.timeralarm,
-  unit1.form1.timerclock,
-  unit1.form1.timerdate,
-  unit1.form1.timergame,
-  unit1.form1.timergameover,
-  unit1.form1.timergamestart,
-  unit1.form1.timerreprise
-}
+unit1.form1.oncreate()
 
 unit1.pfgameover.data = system.loadbin( 'Gameover.pcm' )
 unit1.pfgot.data = system.loadbin( 'Got.pcm' )
@@ -32,17 +10,66 @@ unit1.pfmiss.data = system.loadbin( 'Miss.pcm' )
 unit1.pfmove.data = system.loadbin( 'Move.pcm' )
 unit1.pfstart.data = system.loadbin( 'Start.pcm' )
 
-unit1.form1.oncreate()
-
-unit1.form1.vid_showall( true )
 unit1.bsound = true
 
-return system.init(
-  unit1.form1.im_background,
-  keymap,
-  function( key ) unit1.form1.onkeydown( nil, key, 0 ) end,
-  function( key ) unit1.form1.onkeyup( nil, key, 0 ) end,
-  timers,
-  { 152, 50, 257, 195 },
-  menu
-)
+-- this game doesn't have a acl mode so we have to reblit all images
+do
+  unit1.form1.vid_showall( true )
+end
+
+local zoom = { left = 152, top = 50, width = 257, height = 195 }
+local left, right = system.splith( zoom )
+
+return system.init{
+  background = unit1.form1.im_background,
+  zoom = zoom,
+
+  controls = {
+    {
+      button = unit1.form1.btn_game_a_top,
+      label  = 'Game A'
+    },
+    {
+      button = unit1.form1.btn_game_b_top,
+      label  = 'Game B'
+    },
+    {
+      button = unit1.form1.btn_mode_top,
+      label  = 'Time'
+    },
+    {
+      button = unit1.form1.btn_left_down,
+      zone   = left,
+      label  = 'Left',
+      keys   = { left = true },
+      xkeys  = { forms.vk_left }
+    },
+    {
+      button = unit1.form1.btn_right_down,
+      zone   = right,
+      label  = 'Right',
+      keys   = { right = true, a = true },
+      xkeys  = { forms.vk_right }
+    }
+  },
+
+  timers = {
+    unit1.form1.timeralarm,
+    unit1.form1.timerclock,
+    unit1.form1.timerdate,
+    unit1.form1.timergame,
+    unit1.form1.timergameover,
+    unit1.form1.timergamestart,
+    unit1.form1.timerreprise
+  },
+
+  onkey = function( key, pressed )
+    local handler = pressed and unit1.form1.onkeydown or unit1.form1.onkeyup
+    handler( nil, key, 0 )
+  end,
+
+  onbutton = function( button, pressed )
+    local handler = pressed and button.onmousedown or button.onmouseup
+    handler( nil, controls.mbleft, false, 0, 0 )
+  end
+}
