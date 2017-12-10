8e65e0f60eec6eae11ad181126518545517fa999
leiradel <andre@leiradella.com>
2017-12-10 12:28:40 +0000

    Updated Baseball to support mouse and touches

--- games/baseball/main.lua
+++ games/baseball/main.lua
@@ -1,34 +1,8 @@
 local unit1 = system.loadunit 'unit1'
 local forms = system.loadunit 'forms'
+local controls = system.loadunit 'controls'
 
-local keymap = {
-  left  = { forms.vk_left,  'Left' },
-  right = { forms.vk_right, 'Right' },
-  a     = { forms.vk_right, 'Right' },
-  l1    = { 49,             'Game A' },
-  r1    = { 50,             'Game B' },
-  l2    = { 51,             'Time' },
-  r2    = { 53,             'Light' }
-}
-
-local menu = {
-  { unit1.form1.btn_mode_down,   'Time',   51 },
-  { unit1.form1.btn_game_a_down, 'Game A', 49 },
-  { unit1.form1.btn_game_b_down, 'Game B', 50 },
-  { unit1.form1.btn_light_down,  'Light',  53 }
-}
-
-local timers = {
-  unit1.form1.timer_bonus,
-  unit1.form1.timer_clock,
-  unit1.form1.timer_date,
-  unit1.form1.timer_game,
-  unit1.form1.timer_game_over,
-  unit1.form1.timer_game_reprise,
-  unit1.form1.timer_game_start,
-  unit1.form1.timer_got,
-  unit1.form1.timer_startup
-}
+unit1.form1.oncreate()
 
 unit1.pfgameover.data = system.loadbin( 'Gameover.pcm' )
 unit1.pfgot.data = system.loadbin( 'Got.pcm' )
@@ -36,18 +10,71 @@ unit1.pfmiss.data = system.loadbin( 'Miss.pcm' )
 unit1.pfmove.data = system.loadbin( 'Move.pcm' )
 unit1.pfstart.data = system.loadbin( 'Start.pcm' )
 
-unit1.form1.oncreate()
-
 unit1.bsound = true
 unit1.imode = 0
 unit1.form1.gam_set_mode()
 
-return system.init(
-  unit1.form1.im_background,
-  keymap,
-  function( key ) unit1.form1.onkeydown( nil, key, 0 ) end,
-  function( key ) unit1.form1.onkeyup( nil, key, 0 ) end,
-  timers,
-  { 200, 100, 159, 206 },
-  menu
-)
+local zoom = { left = 200, top = 100, width = 159, height = 206 }
+local left, right = system.splith( zoom )
+
+return system.init{
+  background = unit1.form1.im_background,
+  zoom = zoom,
+
+  controls = {
+    {
+      button = unit1.form1.btn_mode_top,
+      label  = 'Time'
+    },
+    {
+      button = unit1.form1.btn_game_a_top,
+      label  = 'Game A'
+    },
+    {
+      button = unit1.form1.btn_game_b_top,
+      label  = 'Game B'
+    },
+    {
+      button = unit1.form1.btn_light_top,
+      label  = 'Light'
+    },
+    {
+      button = unit1.form1.btn_left_down,
+      zone   = left,
+      label  = 'Left',
+      lbl_dy = 32,
+      keys   = { left = true },
+      xkeys  = { forms.vk_left }
+    },
+    {
+      button = unit1.form1.btn_right_down,
+      zone   = right,
+      label  = 'Right',
+      lbl_dy = 32,
+      keys   = { right = true, a = true },
+      xkeys  = { forms.vk_right }
+    }
+  },
+
+  timers = {
+    unit1.form1.timer_bonus,
+    unit1.form1.timer_clock,
+    unit1.form1.timer_date,
+    unit1.form1.timer_game,
+    unit1.form1.timer_game_over,
+    unit1.form1.timer_game_reprise,
+    unit1.form1.timer_game_start,
+    unit1.form1.timer_got,
+    unit1.form1.timer_startup
+  },
+
+  onkey = function( key, pressed )
+    local handler = pressed and unit1.form1.onkeydown or unit1.form1.onkeyup
+    handler( nil, key, 0 )
+  end,
+
+  onbutton = function( button, pressed )
+    local handler = pressed and button.onmousedown or button.onmouseup
+    handler( nil, controls.mbleft, false, 0, 0 )
+  end
+}
