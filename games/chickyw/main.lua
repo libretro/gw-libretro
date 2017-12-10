b63ac7d46b7303e4870c8bd4f1058aecd9d9de58
leiradel <andre@leiradella.com>
2017-12-10 20:50:06 +0000

    Fixed initialization

--- games/chickyw/main.lua
+++ games/chickyw/main.lua
@@ -39,6 +39,16 @@ unit1.pfpower2.data = system.loadbin( 'Power2.pcm' )
 unit1.pfstart.data = system.loadbin( 'Start.pcm' )
 unit1.pfstgstart.data = system.loadbin( 'Stgstart.pcm' )
 
+-- Fix structures
+do
+  for i = 0, 6 do
+    for j = 0, 8 do
+      unit1.alabyrinth[ i ][ j ].chicky = {}
+      unit1.alabyrinth[ i ][ j ].wolf = {}
+    end
+  end
+end
+
 unit1.form1.oncreate()
 
 unit1.bsound = true
